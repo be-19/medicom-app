54ea5733bc6322167997ad46c3fa6f0b8b777d72
Andrian Ramadan <ramadanandrian89@gmail.com>
2022-10-19 21:32:07 +0700

    menambahkan fitur untuk tampil pasien

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 async function getNakes() {
-  let url = "https://634fe28278563c1d82b28ff3.mockapi.io/nakes";
-  let response = await fetch(url);
-  let data = await response.json();
-  data.forEach((item) => {
-    console.log(item);
-  });
+  let url = "https://634fe28278563c1d82b28ff3.mockapi.io/pasien";
+  let response = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let i = 1;
+      let content = "";
+      data.forEach((item) => {
+        content += `<tr>
+                        <th scope="row">${i++}</th>
+                        <td>${item["nik"]}</td>
+                        <td>${item["name"]}</td>
+                        <td>${item["jenis_kelamin"]}</td>
+                        <td>${item["tanggal_lahir"]}</td>
+                        <td>${item["alamat"]}</td>
+                        <td>${item["no_telp"]}</td>
+                    </tr>`;
+      });
+
+      document.getElementById("table-body").innerHTML = content;
+    });
 }
 
-getNakes();
+window.addEventListener("load", () => {
+  getNakes();
+});
